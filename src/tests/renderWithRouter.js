1d47ffea826c4b2e0d0f5268d9df70bdd75cfc7d
Matheus <matheuseduardo.jp@gmail.com>
2022-08-16 17:51:38 -0300

    Teste App OK

--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
+// Commit
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
