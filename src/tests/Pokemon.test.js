ed0b1afd7ac18eebe63faa02668c66783703d91c
Matheus <matheuseduardo.jp@gmail.com>
2022-08-17 01:02:22 -0300

    Teste PokemonDetails OK

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <Pokemon.js />.', () => {
+  test('01.Teste se é renderizado um card com as informações de determinado pokémon.',
+    () => {
+      renderWithRouter(<App />);
+      const h2 = screen.getByRole(
+        'heading', { level: 2, name: /Encountered pokémons/i },
+      );
+      expect(h2).toBeInTheDocument();
+    });
+  // test('02.Teste se a Pokédex tem os botões de filtro.', () => {
+  //   renderWithRouter(<App />);
+  //   const types = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+  //   const btns = screen.getAllByTestId('pokemon-type-button');
+  //   btns.forEach((btn, index) => expect(btn).toHaveTextContent(types[index]));
+  // });
+  // test('03.O botão All precisa estar sempre visível.', () => {
+  //   renderWithRouter(<App />);
+  //   const btn = screen.getByText(/All/i);
+  //   userEvent.click(btn);
+  // });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,64 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <PokemonDetails.js />', () => {
+  test('01.É exibido na tela um h2 com o texto <name> Details.',
+    () => {
+      renderWithRouter(<App />);
+      const btn = screen.getByText(/More details/i);
+      userEvent.click(btn);
+      const h2 = screen.getByRole(
+        'heading', { level: 2, name: /Pikachu Details/i },
+      );
+      expect(h2).toBeInTheDocument();
+    });
+  test('02.É exibido na tela um h2 com o texto Summary.', () => {
+    renderWithRouter(<App />);
+    const btn = screen.getByText(/More details/i);
+    userEvent.click(btn);
+    const h2 = screen.getByRole(
+      'heading', { level: 2, name: /Summary/i },
+    );
+    expect(h2).toBeInTheDocument();
+    expect(h2).toHaveTextContent('Summary');
+  });
+  test('03.É exibido na tela um h2 com o texto Game Locations of <name>.', () => {
+    renderWithRouter(<App />);
+    const btn = screen.getByText(/More details/i);
+    userEvent.click(btn);
+    const h2 = screen.getByRole(
+      'heading', { level: 2, name: /Game Locations/i },
+    );
+    expect(h2).toBeInTheDocument();
+    expect(h2).toHaveTextContent('Game Locations of Pikachu');
+  });
+  test('04.É exibido na tela uma label com o texto Pokémon favoritado?.', () => {
+    renderWithRouter(<App />);
+    const btn = screen.getByText(/More details/i);
+    userEvent.click(btn);
+    const label = screen.getByLabelText(/Pokémon favoritado/i);
+    expect(label).toBeInTheDocument();
+  });
+  test('05.São exibidas na tela imagens de localização com o src correto.', () => {
+    renderWithRouter(<App />);
+    const btn = screen.getByText(/More details/i);
+    userEvent.click(btn);
+    const urls = ['https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png', 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png'];
+    const imgs = screen.getAllByRole('img', {
+      name: /Pikachu location/i,
+    });
+    imgs.forEach((img, index) => expect(img.src).toEqual(urls[index]));
+  });
+  test(`06.A seção de detalhes deve conter um parágrafo com o resumo
+  do pokémon específico sendo visualizado.`, () => {
+    renderWithRouter(<App />);
+    const btn = screen.getByText(/More details/i);
+    userEvent.click(btn);
+    const paragraph = screen.getByText(/This intelligent Pokémon roasts hard/i);
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph).toHaveTextContent(/This intelligent Pokémon roasts hard/i);
+  });
+});
